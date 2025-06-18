c81c781a45ef363915b3f59a7497f52a0ebd18e6
melekabbes <melik.venom@gmail.com>
2025-06-18 03:24:20 +0200

    Update server.js

--- projet-finale/server.js
+++ projet-finale/server.js
@@ -73,5 +73,5 @@ db.sequelize.sync({ alter: true }).then(() => {
 // t5adem serveur
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-   console.log(Server is running on port ${PORT}.);
+   console.log(`Server is running on port ${PORT}.`);
 });

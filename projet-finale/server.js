0719831e4dfce6a125256607cc4045816423f543
melekabbes <melik.venom@gmail.com>
2025-06-18 03:20:46 +0200

    Update server.js

--- projet-finale/server.js
+++ projet-finale/server.js
@@ -7,15 +7,15 @@ const methodOverride = require('method-override');
 //melek
 const app = express();
 
-// Middleware CORS (ajuste l'origine si besoin)
+// middleware CORS
 app.use(cors({ origin: "http://localhost:8081" }));
 
-// Parser JSON et URL-encoded
+// parser JSON et URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //ahmed
-// Pour supporter PUT/DELETE via _method dans form
+// bech nista3mlo PUT/DELETE bil _method fi form
 app.use(methodOverride('_method'));
 //melek
 // Servir les fichiers statiques (CSS, JS, images génériques) dans /app
@@ -24,31 +24,31 @@ app.use(express.static(path.join(__dirname, "app")));
 // Servir le dossier uploads pour les images des films
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 //melek
-// Configuration du moteur de vues
+// Conf les moteur mta3 vues
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "app", "views"));
 
-// Import des modèles Sequelize
+// jiben les modèles Sequelize
 const db = require("./app/models");
 //ahmed(salle,reservation)+melek(seance)+rihab(le reste)
-// Import des routes
+// jiben les routes
 const moderatorRoutes = require("./app/routes/moderator.routes");
 const filmRoutes = require("./app/routes/film.routes");
 const reservationRoutes = require('./app/routes/reservation.routes');
 const paymentRoutes = require('./app/routes/payments.routes');
 //ahmed(salle,reservation)+melek(seance)+rihab(le reste)
-// Utilisation des routes
+// isti3mel routes
 app.use("/moderator", moderatorRoutes);
 app.use("/", filmRoutes); // tes routes films ici
 app.use('/reservations', reservationRoutes);
 app.use('/payments', paymentRoutes);
 // melek
-// Autres routes API JSON
+// routes API JSON o5rin
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 require("./app/routes/page.routes")(app);
 
-// Synchroniser la base de donnees et creer les rôles par défaut si besoin
+// n3adiw synchronisation mta3 base de données bi role
 db.sequelize.sync({ alter: true }).then(() => {
     console.log("Database synced");
 
@@ -70,8 +70,8 @@ db.sequelize.sync({ alter: true }).then(() => {
     console.error("Error during DB setup:", err);
   });
 
-// Demarrage du serveur
+// t5adem serveur
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+   console.log(Server is running on port ${PORT}.);
 });
